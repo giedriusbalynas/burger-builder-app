8e16a0b22bedb131ca4f3a4e896283f93bcfe03b
giedriusbalynas <58655993+giedriusbalynas@users.noreply.github.com>
2020-03-26 10:44:31 +0200

    Responsive design + side drawer done.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,9 +2,12 @@ import React from 'react';
 import burgerLogo from '../../assets/images/logo.png';
 import styles from './Logo.css';
 
-const logo = props => (
-    <div className={styles.Logo}>
-        <img src={burgerLogo} alt="MyBurger" />
+const logo = (props) => (
+    <div
+        className={styles.Logo}
+        // style={{height: props.height}} (example for using in-line styles with props)
+    >
+        <img src={burgerLogo} alt="MyBurger"/>
     </div>
 );
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,9 +6,11 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 const toolbar = props => (
     <header className={styles.Toolbar}>
         <div>Menu</div>
-        <Logo />
-        <nav>
-            <NavigationItems />
+        <div className={styles.Logo}>
+            <Logo/>
+        </div>
+        <nav className={styles.DesktopOnly}>
+            <NavigationItems/>
         </nav>
     </header>
 );
